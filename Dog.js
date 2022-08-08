168aeeda1ae4ea29087a9ce22d1277fcc127f815
William Leung <gwilliamleung@gmail.com>
2022-08-07 21:21:55 -0400

    WIP

--- Dog.js
+++ Dog.js
@@ -1,1 +1,20 @@
-// Create the Dog class here
+class Dog{
+    constructor(data){
+        Object.assign(this,data)
+    }
+    getInferfaceHtml(){
+        const{name,avatar,age,bio,hasBeenSwiped,hasBeenLiked} = this;
+            return `           
+                <div id="bio" class="container">
+                    <h1 id="name">${name}, ${age}</h1>
+                    <h2>${bio}</h2>
+                </div>
+                <div id="buttons" class="container">
+                    <button id="nopeBtn"><img src="images/icon-cross.png"></button>
+                    <button id="likeBtn"><img src="images/icon-heart.png"></button>
+                </div>`
+     
+    }
+}
+
+export default Dog
--- scripts.js
+++ scripts.js
@@ -1,4 +1,27 @@
-document.getElementById("btn").addEventListener("click",function(){
-    console.log("HELLO!")
+import dogs from './data.js'
+import Dog from './Dog.js';
+
+const likeButton = document.getElementById("likeBtn")
+const nopeButton = document.getElementById("nopeBtn")
+const interfaceEl = document.getElementById("interface")
+
+
+
+likeButton.addEventListener("click",function(){
+    console.log("YES")         
+
 });
 
+
+nopeButton.addEventListener("click",function(){
+    console.log(dogs[1])
+});
+
+function render(){
+    const nextDog = dogs.shift()
+    const currentDog = new Dog(nextDog)
+    interfaceEl.innerHTML = " "
+    interfaceEl.innerHTML = currentDog.getInferfaceHtml
+}
+
+render()
